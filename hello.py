386eadf99706bb010c2eeb446960a98c96f1090b
Diman4a12 <Diman4a65@gmail.com>
2022-06-24 10:12:26 +0500

    WSGI

---
+++ hello.py
@@ -0,0 +1,11 @@
+from email.quoprimime import body_check
+
+
+def application(env, start_response):
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+    body = [(i + '\n') for i in env.QUERY_STRING.split('&')]
+    start_response(status, headers)
+    return [body]
