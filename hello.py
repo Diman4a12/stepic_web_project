3762b8202b4771586105f1d18c0da502993d6902
Diman4a12 <Diman4a65@gmail.com>
2022-06-24 15:30:17 +0500

    is github

--- hello.py
+++ hello.py
@@ -1,8 +1,13 @@
-def application(env, start_response):
-    status = '200 OK'
-    headers = [
-        ('Content-Type', 'text/plain')
-    ]
-    body = [(i + '\r\n') for i in env['QUERY_STRING'].split('&')]
-    start_response(status, headers)
-    return [body]
+#def application(env, start_response):
+#    status = '200 OK'
+#    headers = [
+#        ('Content-Type', 'text/plain')
+#    ]
+#    body = [(i + '\r\n') for i in env['QUERY_STRING'].split('&')]
+#    start_response(status, headers)
+#    return [body]
+
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
