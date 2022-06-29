43f2844f1aea3e07f14362a01fba56453ab2a2ee
Diman4a12 <Diman4a65@gmail.com>
2022-06-29 13:08:27 +0500

    Django conf

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
     """
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns(
-    
+urlpatterns = patterns(    
     url(r'^$', 'qa.views.test'),
     url(r'^login/', 'qa.views.test'),
     url(r'^signup/', 'qa.views.test'),
