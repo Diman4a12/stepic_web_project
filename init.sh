04f380910f9b5be96beb38bc4751487c02a1c40f
Diman4a12 <Diman4a65@gmail.com>
2022-06-29 13:58:16 +0500

    Django conf

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo gunicorn -c /home/box/web/etc/gunicorn-wsgi.conf hello:wsgi_application
-sudo gunicorn -c /home/box/web/etc/gunicorn-wsgi_django.conf ask.wsgi:application
+sudo ln   -sf   /home/box/web/etc/gunicorn.ask.conf      /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
