e725ac41a46721d6f25a0eed0e19596149de4dfb
Diman4a12 <Diman4a65@gmail.com>
2022-06-30 13:17:34 +0500

    Django conf

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+from django.http import HttpResponse
 
-def test(request, *args, **kwargs):
+
+def home(request, *args, **kwargs):
+    response = "Project AskPupkin homepage"
+    return HttpResponse(response)
+
+
+def stub(request, *args, **kwargs):
     return HttpResponse('OK')
 
-# Create your views here.
+
+def question(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
